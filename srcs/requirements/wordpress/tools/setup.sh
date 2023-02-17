b2e1097a1583fcc92afb7249439c0be0edc08e62
Storm De quaij <sde-quai@f1r2s17.codam.nl>
2023-02-17 13:28:43 +0100

    added commit

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -2,11 +2,11 @@
 
 # https://blog.sucuri.net/2022/11/wp-cli-how-to-install-wordpress-via-ssh.html
 cd var/www/html
-wp core download --alow-root
+wp core download --allow-root
 
 wp core config --dbhost=WordPress --dbname=WordPress \
 --dbuser=${WP_ADMIN_USER} --dbpass=${WP_ADMIN_PASSWORD}
-chmod 600 wp-config.php --alow-root
+chmod 600 wp-config.php --allow-root
 
 wp core install --url=${DOMAIN_NAME} --title="Hallo Wereld!" \
 --admin_name=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} \

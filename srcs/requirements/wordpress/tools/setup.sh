7d84e9abe1cdf9b7b11ce685e62337116eca6e35
sde-quai <stormdequay@gmail.com>
2023-06-14 16:13:58 +0200

    added commit

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -4,7 +4,6 @@ set -x
 echo "-----------START script for wordpress--------------"
 cd /var/www/html
 # https://blog.sucuri.net/2022/11/wp-cli-how-to-install-wordpress-via-ssh.html
-wp core download --allow-root;
 
 # wp config create --dbhost=WordPress --dbname=WordPress --dbuser=${WP_ADMIN_USER} --dbpass=${WP_ADMIN_PASSWORD} --allow-root;
 
@@ -17,11 +16,12 @@ until mysqladmin -h${MDB_NAME} -u${WP_USER} -p${WP_PASSWORD} --silent ping
 do
 	sleep 10
 done
+wp core download --allow-root;
 wp core install --url=${DOMAIN_NAME} --title="Hallo Wereld!" \
---admin_name=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} \
---admin_email=kidsachterderits@42.fr --allow-root;
+--admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} \
+--admin_email=${WP_EMAIL} --allow-root;
 
-wp user create ${WP_USER} emaillalala@42.fr --user-pass=${WP_PASSWORD} --allow-root;
+wp user create ${WP_USER} ${WP_EMAIL} --role=author --user-pass=${WP_PASSWORD} --allow-root;
 
 exec /usr/sbin/php-fpm7.3 --nodaemonize
 echo "-----------END script for wordpress--------------"

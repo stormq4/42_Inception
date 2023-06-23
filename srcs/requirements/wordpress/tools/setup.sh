b3c505594a5eb725fcd4060d92b400c4a0b478c2
sde-quai <stormdequay@gmail.com>
2023-06-23 11:42:38 +0200

    added commit

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -8,9 +8,9 @@ while ! mysql -h$MDB_HOST -u$WP_USER -p$WP_PASSWORD $WP_DB_NAME & > /dev/null
 do
 	sleep 5
 done
-# wp core download --allow-root;
+wp core download --allow-root;
 
-# sleep 5
+sleep 5
 # wp config create --dbname=$WP_DB_NAME --dbuser=$WP_USER --dbpass=$WP_PASSWORD --dbhost=$MDB_HOST --alow-root
 # wp core install --url=$DOMAIN_NAME/wordpress --title="Hallo Wereld!" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root;
 

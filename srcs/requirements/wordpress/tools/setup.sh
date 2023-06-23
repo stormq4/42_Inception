a064db68314207a3178c4c0daee33164e4bdd33b
sde-quai <stormdequay@gmail.com>
2023-06-23 11:56:09 +0200

    added commit

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -15,7 +15,7 @@ sleep 30
 wp config create --dbname=$WP_DB_NAME --dbuser=$WP_USER --dbpass=$WP_PASSWORD --dbhost=$MDB_HOST --allow-root
 wp core install --url=$DOMAIN_NAME/wordpress --title="Hallo Wereld!" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root;
 
-# wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWORD --allow-root
+wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWORD --allow-root
 
-# exec php-fpm8 --nodaemonize
+exec php-fpm8 --nodaemonize
 # echo "-----------END script for wordpress--------------"

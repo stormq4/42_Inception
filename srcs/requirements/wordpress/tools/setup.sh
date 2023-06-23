94c8953307b8d837504df94fba75c3dc5f8681b0
sde-quai <stormdequay@gmail.com>
2023-06-23 13:42:06 +0200

    added commit

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -7,13 +7,14 @@ echo "-----------START script for wordpress------------"
 if [ ! -f "/var/www/html/index.php" ]
 then
 	echo "------STAR installing wordpress ------"
-	wp core download --allow-root;
-	sleep 10;
-	while ! mysql -h$MDB_HOST -u$WP_USER -p$WP_PASSWORD $WP_DB_NAME & > /dev/null
+	while ! mysql -h$MDB_HOST -u$WP_USER -p$WP_PASSWORD $WP_DB_NAME & 
 	do
+	# > /dev/null
 		sleep 3
 	done
 	echo "wordpress Connected to mariadb"
+	wp core download --allow-root;
+	sleep 10;
 
 	wp config create --dbname=$WP_DB_NAME --dbuser=$WP_USER --dbpass=$WP_PASSWORD --dbhost=$MDB_HOST --allow-root;
 	wp core install --url=$DOMAIN_NAME/wordpress --title="Hallo Wereld!" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root;

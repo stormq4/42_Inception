dbaa58fd293ecc4db898f72fdc16197ba6aedc15
sde-quai <stormdequay@gmail.com>
2023-06-26 14:12:17 +0200

    added commit

---
+++ srcs/requirements/nginx/tools/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Kamaaan"
+
+if [ ! -f /etc/nginx/ssl/storm.crt ]
+then
+	echo "Configuring new OPENSSL certificate"
+	openssl req \
+	-nodes \
+	-new \
+	-newkey rsa:2048 \
+	-sha256 \
+	-x509 \
+	-days 365 \
+	-out /etc/ssl/certs/certificate.crt \
+	-keyout /etc/ssl/private/certificate.key \
+	-subj "/C=NL/ST=Noord-Holland/L=Amsterdam/O=Codam/CN=sde-quai.42.fr"
+else
+	echo "OPENSSL certificate present"
+fi
+
+exec $@
--- srcs/requirements/nginx/tools/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Kamaaan"
-
-if [ ! -f /etc/nginx/ssl/storm.crt ]
-then
-	echo "not good"
-else
-	echo "not this problem"
-fi

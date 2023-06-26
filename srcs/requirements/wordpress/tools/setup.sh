1084c561c2931b301507d4dd985b7d16c63c44c9
sde-quai <stormdequay@gmail.com>
2023-06-26 14:40:47 +0200

    added commit

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -4,8 +4,7 @@ set -x
 echo "-----------START script for wordpress------------"
 # https://blog.sucuri.net/2022/11/wp-cli-how-to-install-wordpress-via-ssh.html
 
-if [ ! -f "/var/www/html/index.php" ]
-then
+if [ ! -f "/var/www/html/index.php" ]; then
 	echo "------START Connecting to database------"
 	while mariadb -h$MDB_HOST -u$WP_USER -p$WP_PASSWORD 
 	do
@@ -15,12 +14,29 @@ then
 	echo "------wordpress Connected to mariadb------"
 	echo "------Installing Wordpress------"
 	wp core download --allow-root;
-	# sleep 10;
 
-	wp config create --dbname=$WP_DB_NAME --dbuser=$WP_USER --dbpass=$WP_PASSWORD --dbhost=$MDB_HOST --allow-root;
-	wp core install --url=$DOMAIN_NAME/wordpress --title="Hallo Wereld!" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root;
+	wp config create \
+	--dbname=$WP_DB_NAME \
+	--dbuser=$WP_USER \
+	--dbpass=$WP_PASSWORD \
+	--dbhost=$MDB_HOST \
+	--allow-root;
+	
+	wp core install \
+	--url=$DOMAIN_NAME \
+	--title="Hallo Wereld!" \
+	--admin_user=$WP_ADMIN_USER \
+	--admin_password=$WP_ADMIN_PASSWORD \
+	--admin_email=$WP_ADMIN_EMAIL \
+	--skip-email \
+	--allow-root;
 
-	wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWORD --allow-root;
+	wp user create \
+	$WP_USER $WP_EMAIL \
+	--role=author \
+	--user_pass=$WP_PASSWORD \
+	--allow-root;
+
 	echo "------Wordpress Installed------"
 else
 	echo "------wordpress already present------"

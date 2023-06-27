48b0413eaff2bc14ab2be8026b03328c2d4c702e
sde-quai <stormdequay@gmail.com>
2023-06-27 10:25:51 +0200

    added commit

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -28,12 +28,12 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 	--title="Hallo Wereld!" \
 	--admin_user=$WP_ADMIN_USER \
 	--admin_password=$WP_ADMIN_PASSWORD \
-	--admin_email=$WP_ADMIN_EMAIL \
+	--admin_email=$WP_ADMIN_USER@$DOMAIN_NAME \
 	--skip-email \
 	--allow-root;
 
 	wp user create \
-	$WP_USER $WP_EMAIL \
+	$WP_USER $WP_USER@$DOMAIN_NAME \
 	--role=author \
 	--user_pass=$WP_PASSWORD \
 	--allow-root;
